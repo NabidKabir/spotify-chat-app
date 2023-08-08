1a4000e31c6d3278b06756d7d6def234c7b2dbec
NabidKabir <nabid.kabir312@gmail.com>
2023-08-08 09:33:02 -0400

    added database code

---
+++ server/database.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE songchatapp;
+
+CREATE TABLE users(
+    user_id SERIAL PRIMARY KEY,
+    email VARCHAR(100) NOT NULL,
+    display_name VARCHAR(100) NOT NULL,
+    spotify_token VARCHAR(200) NOT NULL,
+    current_song_id VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE thread(
+    thread_id SERIAL PRIMARY KEY,
+    song_id VARCHAR(100)
+);
+
+CREATE TABLE participants(
+    participant_id SERIAL PRIMARY KEY
+    user_id INT REFERENCES users(user_id)
+    thread_id INT REFERENCES thread(thread_id)
+);
+
+CREATE TABLE messages(
+    message_id SERIAL PRIMARY KEY,
+    sender_id INT REFERENCES users(user_id)
+    thread_id INT REFERENCES threads(thread_id)
+    content TEXT
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
